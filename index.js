6fa44e869e1efa247b62024d38a8c277ac6414fe
PaccyC <abayisengaaimepacifique@gmail.com>
2026-05-29 15:54:59 +0200

    initial setup of npm package

---
+++ index.js
@@ -0,0 +1,15 @@
+export class Greetings{
+
+
+    static sayHello(name){
+      return `Hello, ${name}. How are you?`
+    }
+
+    static reply(name){
+        return `I am doing great, ${name}. How old are you`
+    }
+
+    static sayAge(age){
+        return `I am ${age} years old.`
+    }
+}
